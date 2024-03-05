cb0c4be9118c5f67eabbbabb6ee2515328be6fa3
Isro Hidayatulloh <orsi.amax@gmail.com>
2024-03-05 19:54:12 +0700

    testing

---
+++ lib/class_in_dart/book.dart
@@ -0,0 +1,12 @@
+class Book {
+  String? name;
+  String? author;
+  double? price;
+  int? year;
+
+  void display() {
+    print("Book title is $name.");
+    print("Author name is $author.");
+    print("The book has price $price.");
+  }
+}
---
+++ lib/class_in_dart/testing.dart
@@ -0,0 +1,9 @@
+import 'book.dart';
+
+void main() {
+  Book book = Book();
+  book.name = "The Red Wings";
+  book.author = "Harri Stampson";
+  book.price = 45.0;
+  book.display();
+}
